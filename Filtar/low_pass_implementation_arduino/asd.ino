62a4940c4dff2a1e855ef977f724eb63ef396486
Blagoje Blagojevic <blagojeblagojevic562@gmail.com>
2023-02-01 16:48:43 +0100

    Update asd.ino

--- Filtar/low_pass_implementation_arduino/asd.ino
+++ Filtar/low_pass_implementation_arduino/asd.ino
@@ -16,7 +16,7 @@ void loop() {
 
   float xn = sin(2*PI*2*t) + 0.2*sin(2*PI*50*t)+sin(2*PI*30*t);
 
-  // Compute the filtered signal
+  // Filtrirani signal koeficijenti u notebooku 
   float yn = 0.969*yn1 + 0.0155*xn + 0.0155*xn1;
 
   delay(1);
